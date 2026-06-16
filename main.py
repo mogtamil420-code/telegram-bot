074d9a3c972dc2af0914f54d4bcda7970a9432c7
mogtamil420-code <mogtamil420@gmail.com>
2026-06-16 19:33:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,30 +10,16 @@ client = MongoClient(MONGO_URL)
 db = client["autofilter"]
 files = db["files"]
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is running 🚀")
 
-
-# Save files from channel
 async def save_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.channel_post and update.channel_post.document:
-        file = update.channel_post.document
-
-        files.insert_one({
-            "file_name": file.file_name,
-            "file_id": file.file_id
-        })
-
-        print(f"Saved: {file.file_name}")
-
+    print("UPDATE RECEIVED")
+    print(update)
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-
-app.add_handler(
-    MessageHandler(filters.Document.ALL, save_file)
-)
+app.add_handler(MessageHandler(filters.ALL, save_file))
 
 app.run_polling()
