4e8df83df4f2abd70229922d615e849584a739c7
mogtamil420-code <mogtamil420@gmail.com>
2026-06-21 12:50:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ files = db["files"]
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is running 🚀")
 
+async def test(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    movie = files.find_one()
+
+    if movie:
+        await update.message.reply_text(
+            f"Found: {movie['file_name']}"
+        )
+    else:
+        await update.message.reply_text("No files found")
+
 async def save_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.channel_post and update.channel_post.document:
         doc = update.channel_post.document
@@ -28,6 +38,7 @@ async def save_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("test", test))
 app.add_handler(MessageHandler(filters.ALL, save_file))
 
 app.run_polling()
